83dfb3e6dff943d7d9046676c227f5b0264fc261
Youngwoo Lee <youngwoo206@gmail.com>
2025-03-02 20:24:14 -0500

    ros2 init

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import { useState, useEffect } from "react";
 import Input from "./components/Input";
+import ROSLIB from "roslib";
 
 function App() {
+  const [connected, setConnected] = useState(false);
+  const [message, setMessage] = useState("");
+  const [ros, setRos] = useState(null);
+
+  const newRos = new ROSLIB.Ros({
+    url: "ws://localhost:9090",
+  });
+
+  console.log("ROS: ", newRos);
+
   return (
     <div className="h-[90vh] flex justify-center">
       <div className="bg-amber-100 w-[90vw] h-40 mt-[10vh]">
